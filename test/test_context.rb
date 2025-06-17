f6e4091cda6c26e0c6861420915fff7050a61ce7
Sharon Rosner <sharon@noteflakes.com>
2025-06-17 20:29:52 +0200

    More work on param validation

--- test/test_context.rb
+++ test/test_context.rb
@@ -4,7 +4,7 @@ require_relative 'helper'
 
 class ContextTest < Minitest::Test
   def setup
-    @req = mock_req(':method' => 'GET', ':path' => '/foo?q=foo&x=bar&y=123')
+    @req = mock_req(':method' => 'GET', ':path' => '/foo?q=foo&x=bar&y=123&z1=t&z2=f')
     @ctx = Syntropy::Context.new(@req)
   end
 
@@ -25,11 +25,16 @@ class ContextTest < Minitest::Test
     assert_equal 123, @ctx.validate_param(:y, Integer, 120..125)
     assert_equal 123.0, @ctx.validate_param(:y, Float)
 
+    assert_equal true, @ctx.validate_param(:z1, :bool)
+    assert_equal false, @ctx.validate_param(:z2, :bool)
+
     assert_raises(VE) { @ctx.validate_param(:azerty, String) }
     assert_raises(VE) { @ctx.validate_param(:q, Integer) }
     assert_raises(VE) { @ctx.validate_param(:q, Float) }
     assert_raises(VE) { @ctx.validate_param(:q, nil) }
     
     assert_raises(VE) { @ctx.validate_param(:y, Integer, 1..100) }
+
+    assert_raises(VE) { @ctx.validate_param(:y, :bool) }
   end
 end
