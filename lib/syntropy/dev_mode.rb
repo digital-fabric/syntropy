3eeb3dbf30d2011060871dbed4d5e76c156bab66
Sharon Rosner <sharon@noteflakes.com>
2025-09-12 09:05:42 +0200

    Change watch mode to dev mode, inject debug HTML attributes in dev mode

---
+++ lib/syntropy/dev_mode.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+TAG_DEBUG_PROC = ->(level, fn, line, col) {
+  {
+    'data-syntropy-level' => level,
+    'data-syntropy-fn'    => fn,
+    'data-syntropy-loc'   => "vscode://file/#{fn}:#{line}:#{col}"
+  }
+}
+
+P2::Compiler.html_debug_attribute_injector = TAG_DEBUG_PROC
