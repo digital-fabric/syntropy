30a74c1f676bba3dfb39fa9a30cf5d719fc0c672
Sharon Rosner <sharon@noteflakes.com>
2026-05-27 11:33:36 +0200

    Implement syntropy test command

---
+++ cmd/test.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+env = {}
+argv_copy = ARGV.dup
+case ARGV[0]
+when '-w', '--watch'
+  env[:watch_mode] = true
+  ARGV.shift
+when '-h', '--help'
+  puts <<~MSG
+    Usage: syntropy test [options] [minitest options]
+        -w, --watch         Rerun tests on file system changes
+        -h, --help          Show this help message
+  MSG
+  exit
+end
+
+require_relative '../lib/syntropy'
+require_relative '../lib/syntropy/test'
+
+$stdout.sync = true
+$stderr.sync = true
+
+Dir.glob("./test/test_*.rb").each { require(it) }
+
+def watch_for_file_changes
+  m = UM.new
+  puts "Waiting for file changes..."
+  m.file_watch('.', UM::IN_CREATE | UM::IN_DELETE | UM::IN_CLOSE_WRITE) { |e|
+    puts "Detected file changes, restarting"
+    break
+  }
+end
+
+Minitest.run ARGV
+if env[:watch_mode]
+  puts
+  watch_for_file_changes
+  exec("ruby", __FILE__, *argv_copy)
+end
--- examples/mcp-oauth/test/helper.rb
+++ examples/mcp-oauth/test/helper.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require 'bundler/setup'
-require 'syntropy'
-require 'syntropy/test'
-require 'minitest/autorun'
+# require 'bundler/setup'
+# require 'syntropy'
+# require 'syntropy/test'
+# require 'minitest/autorun'
 
-STDOUT.sync = true
-STDERR.sync = true
+# STDOUT.sync = true
+# STDERR.sync = true
