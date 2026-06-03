00e559a7241265ad9a577329d5f6f26629bbd6df
Sharon Rosner <sharon@noteflakes.com>
2026-06-03 17:33:16 +0200

    Add template example

--- examples/blog/app/index.rb
+++ examples/blog/app/index.rb
@@ -1,7 +1,17 @@
 layout = import '_layout/default'
 
 export layout.apply {
-  p {
-    a 'Blog posts', href: '/posts'
+  div(style: 'text-align: center; font-size: 2em; font-weight: normal') {
+    h1 {
+      a(
+        'Syntropy',
+        style: 'color: #238',
+        href: 'https://github.com/digital-fabric/syntropy',
+        target: 'none'
+      )
+    }
+    p {
+      a 'Blog posts', href: '/posts'
+    }
   }
 }
--- examples/template/app/_layout/default.rb
+++ examples/template/app/_layout/default.rb
@@ -1,7 +1,7 @@
 export template { |**props|
   html {
     head {
-      title "My awesome blog"
+      title "My awesome Syntropy web app"
       meta charset: 'utf-8'
       meta name: 'viewport', content: 'width=device-width, initial-scale=1.0'
       link rel: "stylesheet", type: "text/css", href: "/assets/style.css"
